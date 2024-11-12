cc1e6415381d22d087171442ba865d879bf33498
unknown <ezekuiel100@gmail.com>
2024-11-12 11:13:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const gallery = document.querySelector("#gallery");
+
+const content = document.querySelector("#content");
+
+const links = [home, about, gallery];
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const page = e.target.id;
+    console.log(page);
+    history.pushState({ page }, "", page);
+
+    renderPage(page);
+  });
+});
+
+async function renderPage(page) {
+  try {
+    const res = await fetch(page + ".html");
+
+    if (!res.ok) {
+      throw new Error();
+    }
+
+    content.innerHTML = await res.text();
+  } catch (error) {
+    content.innerHTML = "<h1>Pagina nao encontrada</h1>";
+  }
+}
+
+window.addEventListener("popstate", (e) => {
+  const page = e.state?.page || "home";
+
+  renderPage(page);
+});
+
+renderPage("home");
