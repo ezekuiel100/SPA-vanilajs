8d8d150182eaddb621918a5a4d23a732ab105fa0
unknown <ezekuiel100@gmail.com>
2024-11-15 16:18:48 -0300

    Card

--- component/MyCard.js
+++ component/MyCard.js
@@ -1,8 +1,19 @@
+const animals = [
+  { animal: "Dog", image: "images/dog.jpg" },
+  { animal: "Cat", image: "images/cat.jpg" },
+  { animal: "Fish", image: "images/fish.jpg" },
+];
+
 class MyCard extends HTMLElement {
   constructor() {
     super();
 
-    this.innerHTML = "<div>OLA MUNDO</div>";
+    this.innerHTML = `
+      <div class="card">
+      ${animals
+        .map((animal) => `<img src=${animal.image} width="250" />`)
+        .join("")}     
+      </div>`;
   }
 }
 
