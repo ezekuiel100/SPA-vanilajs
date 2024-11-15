c7d29017591dd1b60be91d8340fb7952d557c514
unknown <ezekuiel100@gmail.com>
2024-11-15 19:16:29 -0300

    Card

--- component/MyCard.js
+++ component/MyCard.js
@@ -7,13 +7,48 @@ const animals = [
 class MyCard extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    this.innerHTML = `
-      <div class="card">
-      ${animals
-        .map((animal) => `<img src=${animal.image} width="250" />`)
-        .join("")}     
-      </div>`;
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+    <style>
+      .card {
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        padding: 16px;
+        margin: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #f0f0f0;
+      }
+      
+      img {
+        margin-bottom: 10px;
+        border: 1px solid #999;
+      }
+      
+      span {
+        font-size: 18px;
+        font-weight: bold;
+        color: #333;
+      }
+    </style>
+    
+    <div style="display:flex;">
+        ${animals
+          .map(
+            (animal) => `          
+          <div class="card">     
+          <img src=${animal.image} width="250" height="250" />       
+          <span>${animal.animal}</span>    
+          </div>
+          `
+          )
+          .join("")}
+      </div>
+      `;
   }
 }
 
